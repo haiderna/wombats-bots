e2f21d4e7ffe31ea51e2affbe1d080be84b33eab
Najia Haider <haiderna@vcu.edu>
2017-07-07 14:55:49 -0400

    Update javascript-sample.js

--- javascript-sample.js
+++ javascript-sample.js
@@ -12,12 +12,36 @@
  
  const index = Math.floor(Math.random() * 17);
   
-
+    const smokeDirections = ['forward', 'backward', 'left', 'right', 'drop'];
+    const smokeDirection = smokeDirections[Math.floor(Math.random() * 5)];
 
  const command = index % 2 === 0 ? {
          action: 'shoot', metadata: {} 
        
-   } : {action: 'turn', metadata: { direction: 'right'}}
+   } :
+   index % 3 == 0 && index % 2 != 0? 
+   {action: 'move', metadata: { direction: 'forward'}}: 
+       index % 5 == 0 && index % 3  != 0 && index % 2 != 0 ?
+    {action: 'smoke', metadata: {direction: smokeDirection }}: 
+   {action: 'turn', metadata: { direction: direction() }}
+         
+
+
+function direction() {
+    const dir_index = Math.floor(Math.random() * 10);
+    if (dir_index % 2 == 0) {
+        return 'right';
+        
+    }
+    if (dir_index % 2 != 0 && dir_index % 5 == 0){
+        return 'about-face'
+    } 
+    
+     if (dir_index % 2 != 0 && dir_index % 5 != 0){
+        return 'left'
+    } 
+    
+}
 
 function goThrough() {
     
@@ -35,4 +59,5 @@ function goThrough() {
         }
     };
 });
+
 
